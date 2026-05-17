031f0da2d86383b348d0a0c37590674f58caf0a7
JasonArilani <arilanijason@gmail.com>
2026-05-17 16:48:18 -0700

    Update ed_ap_anthony_patch.js

--- ed_ap_anthony_patch.js
+++ ed_ap_anthony_patch.js
@@ -29,8 +29,11 @@ function insertAPFlag(iso, scriptId, index, flag, callArgs = 0) {
 function applyAnthonyAPPreAlphaPatch(iso) {
   console.log("Applying Eternal Darkness Anthony AP Pre-Alpha patch...");
 
-  insertAPFlag(iso, 2129, 230, 1144, 0); // 3 Point Circle
-
+  // 3 Point Circle
+  modifyScript(iso, 2129, s => {
+    s.addFlagSet([[0x80725EB0, 0]]);
+  });
+  
   addAPFlagToEnd(iso, 1360, 1145); // Weak Alignment Rune variant
   addAPFlagToEnd(iso, 1361, 1145); // Weak Alignment Rune variant
   addAPFlagToEnd(iso, 1362, 1145); // Weak Alignment Rune variant
